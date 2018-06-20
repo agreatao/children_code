06a9ca903bf9d8bb85b1e87c9a7d3ff3e878118a
viva <1012188283@qq.com>
2018-06-20 23:32:14 +0800

    新增课程盒子

--- src/components/LessonContainer.vue
+++ src/components/LessonContainer.vue
@@ -11,7 +11,7 @@
                         <span class="lesson-card-icon"></span>
                         <span class="lesson-amount">课程数量</span>
                     </div>
-                <p class="lesson-card-desc"></p>
+                    <p class="lesson-card-desc"></p>
             </div>
         </a>
     </div>
@@ -20,18 +20,73 @@
 
 <script>
 export default {
-    name:'LessonContainer',
-    data(){
-        return{
-            lessonlist:[{},{}],
-        };
-    }
-}
+  name: "LessonContainer",
+  data() {
+    return {
+      lessonlist: [{}, {}]
+    };
+  }
+};
 </script>
 
 <style>
-.lesson-card-container{height: 256px; width: 216px; float: left; margin:0px 0px 18px 18px; background-color: black;}
-
+.lesson-card-container {
+    height: 256px;
+    width: 216px;
+    float: left;
+    margin: 0px 0px 18px 18px;
+    background-color: black;
+}
+.lesson-card-top {
+    width: 216px;
+    height: 120px;
+    transition: all .3s;
+}
+.lesson-card-top img{
+    width: 100%;
+    height: 100%;
+    border-radius: 8px;
+    background-color: #f3f5f7;
+}
+.lesson-card-content{
+    padding: 12px 8px;
+}
+.lesson-card-content .lesson-card-name{
+    font-size: 16px;
+    color: #07111B;
+    line-height: 24px;
+    word-wrap: break-word;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    max-height: 46px;
+    transition: all .3s;
+    }
+.lesson-card-content .lesson-card-info {
+    font-size: 12px;
+    color: #93999F;
+    line-height: 24px;
+    font-weight: 400;
+}
+.lesson-card-container .lesson-card-content .lesson-card-info span {
+    display: inline-block;
+    margin-right: 12px;
+}
+.course-card-container .course-card-content .course-card-desc {
+    font-size: 12px;
+    font-weight: 300;
+    color: #93999F;
+    line-height: 22px;
+    margin-top: 2px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    height: 44px;
+}
 </style>
 
 
