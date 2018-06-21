94090293d5baae95f6770befa62f90c9020be760
Mr.X <512573042@qq.com>
2018-06-21 15:54:28 +0800

    修改导航，固定在顶部

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,16 @@
 <template>
     <div class="footer">
+        <div>
+            <ul class="clearfix">
+                <li>a</li>
+                <li>b</li>
+                <li>c</li>
+            </ul>
+            <ol>
+                <li>d</li>
+                <li>e</li>
+            </ol>
+        </div>
 
     </div>
 </template>
@@ -12,5 +23,26 @@
     .footer {
         min-height: 35px;
         background-color: #222;
+        color: #fff;
+        ul {
+            li {
+                display: block;
+                float: left;
+            }
+            &:before, &:after {
+                clear: both;
+                content: '';
+                visibility: hidden;
+                display: block;
+            }
+
+        }
+        .claerfix {
+
+        }
+
+
     }
+
+
 </style>
